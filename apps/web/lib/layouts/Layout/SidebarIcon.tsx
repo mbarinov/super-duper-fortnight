6fc9b3deabe724b1b03b880a91b0b452ea5a8b21
Max Barinov <me@maxbarinov.com>
2025-02-27 15:34:46 +0400

    feat(web): Enhance sidebar icon hover and active states with subtle animation

--- apps/web/lib/layouts/Layout/SidebarIcon.tsx
+++ apps/web/lib/layouts/Layout/SidebarIcon.tsx
@@ -16,7 +16,7 @@ export const SidebarIcon = ({ children, label, href }: SidebarIconProps) => {
   const isActive = pathname === href;
 
   const iconClassName = classNames(
-    "w-6 h-6 transition-colors duration-200 ease-in-out",
+    "w-6 h-6 transition-colors duration-300 ease-in-out hover:text-[#232323]",
     {
       "text-[#232323]": isActive,
       "text-[#B1B1B1]": !isActive,
@@ -24,7 +24,7 @@ export const SidebarIcon = ({ children, label, href }: SidebarIconProps) => {
   );
 
   const labelClassName = classNames(
-    "transition-colors duration-200 ease-in-out ml-6",
+    "transition-colors duration-200 ease-in-out ml-6 hover:text-[#232323]",
     {
       "text-[#232323]": isActive,
       "text-[#B1B1B1]": !isActive,
@@ -34,10 +34,18 @@ export const SidebarIcon = ({ children, label, href }: SidebarIconProps) => {
   return (
     <Link
       href={href}
-      className="flex items-center px-4 py-2 rounded-md mx-2 transition-colors duration-200 ease-in-out group"
+      className="flex items-center px-4 py-2 rounded-md mx-2 transition-colors duration-200 ease-in-out group relative"
     >
       <div className={iconClassName}>{children}</div>
       <span className={labelClassName}>{label}</span>
+      <div
+        className={classNames(
+          "absolute left-0 top-0 w-[6px] transition-all duration-300 h-full ml-[-10px] bg-[#232323] rounded-full",
+          {
+            "!ml-[-14px]": !isActive,
+          }
+        )}
+      />
     </Link>
   );
 };
